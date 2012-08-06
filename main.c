433f4f647ed824d5a19fd60ee2fb95cfd1eeffb3
Peter Geissler <peter.geissler@cyso.nl>
2012-08-06 10:45:58 +0200

    Fix indenting fuckups

--- main.c
+++ main.c
@@ -25,55 +25,56 @@ signed int delayTime;
 static Z80Context context;
 
 void updateFPS() {
-    if( ( glutGet( GLUT_ELAPSED_TIME )-lastFps ) > 1000 ) {
-        static char buffer[20] = {0};
-        sprintf( buffer, "%d FPS", fps );
+	if( ( glutGet( GLUT_ELAPSED_TIME )-lastFps ) > 1000 ) {
+		static char buffer[20] = {0};
+		sprintf( buffer, "%d FPS", fps );
 
-        glutSetWindowTitle( buffer );
-        fps = 0;
-        lastFps = glutGet( GLUT_ELAPSED_TIME );
-    }
-    fps++;
+		glutSetWindowTitle( buffer );
+		fps = 0;
+		lastFps = glutGet( GLUT_ELAPSED_TIME );
+	}
+
+	fps++;
 }
 
 void initTexture() {
-		int i;
+	int i;
 
-    for( i=0; i<( SCREEN_H_SMS*SCREEN_W_SMS ); i++ ) {
-        screen[i] = 0x0000ff00 | (i^0xff) & 0xff;
-    }
-    // Create a texture
-    glTexImage2D( GL_TEXTURE_2D, 0, 3, SCREEN_W_SMS, SCREEN_H_SMS, 0, GL_BGRA, GL_UNSIGNED_BYTE, ( GLvoid* )screen );
+	for( i=0; i<( SCREEN_H_SMS*SCREEN_W_SMS ); i++ ) {
+		screen[i] = 0x0000ff00 | (i^0xff) & 0xff;
+	}
 
-    // Set up the texture
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP );
+	// Create a texture
+	glTexImage2D( GL_TEXTURE_2D, 0, 3, SCREEN_W_SMS, SCREEN_H_SMS, 0, GL_BGRA, GL_UNSIGNED_BYTE, ( GLvoid* )screen );
 
-    // Enable textures
-    glEnable( GL_TEXTURE_2D );
+	// Set up the texture
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP );
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP );
+
+	// Enable textures
+	glEnable( GL_TEXTURE_2D );
 }
 
 void updateTexture() {
+	// Update Texture
+	glTexSubImage2D( GL_TEXTURE_2D, 0 ,0, 0, SCREEN_W_SMS, SCREEN_H_SMS, GL_BGRA, GL_UNSIGNED_BYTE, ( GLvoid* )screen );
 
-    // Update Texture
-    glTexSubImage2D( GL_TEXTURE_2D, 0 ,0, 0, SCREEN_W_SMS, SCREEN_H_SMS, GL_BGRA, GL_UNSIGNED_BYTE, ( GLvoid* )screen );
+	glBegin( GL_QUADS );
 
-    glBegin( GL_QUADS );
+	glTexCoord2d( 0.0, 0.0 );
+	glVertex2d( 0.0, 0.0 );
 
-    glTexCoord2d( 0.0, 0.0 );
-    glVertex2d( 0.0, 0.0 );
+	glTexCoord2d( 1.0, 0.0 );
+	glVertex2d( sw, 0.0 );
 
-    glTexCoord2d( 1.0, 0.0 );
-    glVertex2d( sw, 0.0 );
+	glTexCoord2d( 1.0, 1.0 );
+	glVertex2d( sw, sh );
 
-    glTexCoord2d( 1.0, 1.0 );
-    glVertex2d( sw, sh );
-
-    glTexCoord2d( 0.0, 1.0 );
-    glVertex2d( 0.0, sh );
-    glEnd();
+	glTexCoord2d( 0.0, 1.0 );
+	glVertex2d( 0.0, sh );
+	glEnd();
 }
 
 void display() {
@@ -114,61 +115,61 @@ void display() {
 	}
 
 	updateTexture();
-  updateFPS();
-  glutSwapBuffers();
+	updateFPS();
+	glutSwapBuffers();
 
-  delayTime = waitTime - (glutGet( GLUT_ELAPSED_TIME ) - frameStartTime);
-  if(delayTime > 0) {
-      usleep((u32)delayTime*1000);
-  }
-  frameStartTime = glutGet( GLUT_ELAPSED_TIME );
+	delayTime = waitTime - (glutGet( GLUT_ELAPSED_TIME ) - frameStartTime);
+	if(delayTime > 0) {
+		usleep((u32)delayTime*1000);
+	}
+	frameStartTime = glutGet( GLUT_ELAPSED_TIME );
 }
 
 void reshape_window( GLsizei w, GLsizei h ) {
-    glClearColor( 0.0f, 0.0f, 0.5f, 0.0f );
-    glMatrixMode( GL_PROJECTION );
-    glLoadIdentity();
-    gluOrtho2D( 0, w, h, 0 );
-    glMatrixMode( GL_MODELVIEW );
-    glViewport( 0, 0, w, h );
+	glClearColor( 0.0f, 0.0f, 0.5f, 0.0f );
+	glMatrixMode( GL_PROJECTION );
+	glLoadIdentity();
+	gluOrtho2D( 0, w, h, 0 );
+	glMatrixMode( GL_MODELVIEW );
+	glViewport( 0, 0, w, h );
 
-    // Resize quad
-    sw = w;
-    sh = h;
+	// Resize quad
+	sw = w;
+	sh = h;
 }
 
 
 void init_emulator() {
-  context.memRead  = mmu_mem_read;
-  context.memWrite = mmu_mem_write;
-  context.ioRead   = mmu_io_read;
-  context.ioWrite  = mmu_io_write;
+	context.memRead  = mmu_mem_read;
+	context.memWrite = mmu_mem_write;
+	context.ioRead   = mmu_io_read;
+	context.ioWrite  = mmu_io_write;
 }
 
 static void reset() {
-  Z80RESET(&context);
+	Z80RESET(&context);
 }
 
 static void dump_z80_state( void ) {
-  printf(
+	printf(
 		"%04x %04x %04x %04x %04x %04x %04x %04x %04x %04x %04x %04x\n",
-	  AF1, BC1, DE1, HL1, AF2, BC2, DE2, HL2, IX, IY, SP, PC
+		AF1, BC1, DE1, HL1, AF2, BC2, DE2, HL2, IX, IY, SP, PC
 	);
 
 	printf(
 		"%02x %02x %d %d %d %d %d\n", I, R,
-	  IFF1, IFF2, IM, context.halted, context.tstates
+		IFF1, IFF2, IM, context.halted, context.tstates
 	);
 }
 
 int main( int argc, char **argv ) {
-  FILE *f;
+	FILE *f;
 	u8 *mem;
 	int f_sz;
 
 	mem = mmu_mem_get();
 
-  f = fopen(argv[1], "r");
+	f = fopen(argv[1], "r");
 	fseek(f,0,SEEK_END);
 	f_sz=ftell(f);
 	fseek(f,0,SEEK_SET);
@@ -176,7 +177,7 @@ int main( int argc, char **argv ) {
 	fread(mem, (f_sz < 1024*32) ? f_sz : 1024*32, 1, f);
 	fclose(f);
 
-  init_emulator();
+	init_emulator();
 	vdp_init(screen);
 
 	// Setup OpenGL
